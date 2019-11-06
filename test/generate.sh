d5ab682c480a63b09722efe3f659b6038c35040c
Donghyun Gouk <kukdh1@gmail.com>
2019-11-06 21:10:22 +0900

    test: Update test script

--- test/generate.sh
+++ test/generate.sh
@@ -2,12 +2,29 @@
 PREFIX="temp_build"
 SOURCE_FILE=$1
 SOURCE_DIR=$(dirname $SOURCE_FILE)
-LLVM_ASSEMBLY=$PREFIX"/"$1".ll"
-LLVM_ASSEMBLY_OPT=$PREFIX"/"$1".opt.ll"
+
+TEXT_MODE=1
+TEXT_FLAG="-S"
+
+if [ $TEXT_MODE -eq "1" ];
+then
+  LLVM_ASSEMBLY=$PREFIX"/"$1".ll"
+  LLVM_ASSEMBLY_OPT=$PREFIX"/"$1".opt.ll"
+else
+  LLVM_ASSEMBLY=$PREFIX"/"$1".bc"
+  LLVM_ASSEMBLY_OPT=$PREFIX"/"$1".opt.bc"
+  TEXT_FLAG=""
+fi
+
 ASSEMBLY=$PREFIX"/"$1".S"
 
 mkdir -p $PREFIX"/"$SOURCE_DIR
 
-clang++ -std=c++17 -DEXCLUDE_CPU -g -O2 -emit-llvm -I. -I../lib/drampower/src -c -o $LLVM_ASSEMBLY $SOURCE_FILE
-opt -march arm -mcpu cortex-r7 -O2 --load ./lib/llvm-simplessd/build/libllvm_simplessd.so --blockcollector -S -o $LLVM_ASSEMBLY_OPT $LLVM_ASSEMBLY
+# Basic Block collection
+clang++ -std=c++17 -DEXCLUDE_CPU -g -O2 -emit-llvm -I. -I../lib/drampower/src -c $TEXT_FLAG -o $LLVM_ASSEMBLY $SOURCE_FILE
+opt -march arm -mcpu cortex-r7 --load ./lib/llvm-simplessd/build/libllvm_simplessd.so --blockcollector $TEXT_FLAG -o $LLVM_ASSEMBLY_OPT $LLVM_ASSEMBLY
 llc -march=arm -mcpu=cortex-r7 -filetype=asm -o $ASSEMBLY $LLVM_ASSEMBLY_OPT
+
+# Instruction count
+
+# Apply instruction statistics
