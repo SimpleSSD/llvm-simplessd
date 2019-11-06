1ee39300d6dc8dc72e916a2c04ccd64cc7fcf703
Donghyun Gouk <kukdh1@gmail.com>
2019-11-06 20:21:20 +0900

    test: Update test script

--- test/generate.sh
+++ test/generate.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-
+PREFIX="temp_build"
 SOURCE_FILE=$1
-LLVM_ASSEMBLY=$1".ll"
-LLVM_ASSEMBLY_OPT=$1".opt.ll"
-ASSEMBLY=$1".S"
+SOURCE_DIR=$(dirname $SOURCE_FILE)
+LLVM_ASSEMBLY=$PREFIX"/"$1".ll"
+LLVM_ASSEMBLY_OPT=$PREFIX"/"$1".opt.ll"
+ASSEMBLY=$PREFIX"/"$1".S"
 
-clang++ -std=c++17 -DDISABLE_LOG -emit-llvm -I. -I../lib/drampower/src -S -c $SOURCE_FILE -o $LLVM_ASSEMBLY
-opt -O2 -S -march arm -mcpu cortex-r7 -o $LLVM_ASSEMBLY_OPT $LLVM_ASSEMBLY
+mkdir -p $PREFIX"/"$SOURCE_DIR
+
+clang++ -std=c++17 -DEXCLUDE_CPU -g -S -emit-llvm -I. -I../lib/drampower/src -c $SOURCE_FILE -o $LLVM_ASSEMBLY
+opt --load ./lib/llvm-simplessd/build/libllvm_simplessd.so --blockcollector -O2 -S -march arm -mcpu cortex-r7 -o $LLVM_ASSEMBLY_OPT $LLVM_ASSEMBLY
 llc -march=arm -mcpu=cortex-r7 -filetype=asm -o $ASSEMBLY $LLVM_ASSEMBLY_OPT
