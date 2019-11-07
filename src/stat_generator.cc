1fdccd82f696ecceee79baa329eebef014c941f7
Donghyun Gouk <kukdh1@gmail.com>
2019-11-07 15:44:14 +0900

    statgen: Add bbinfo parser

--- src/stat_generator.cc
+++ src/stat_generator.cc
@@ -51,6 +51,147 @@ struct Function {
 };
 
 bool loadBasicBlockInfo(std::vector<Function> &list, std::string filename) {
+  std::ifstream file(filename);
+
+  if (!file.is_open()) {
+#ifdef DEBUG_MODE
+    std::cerr << "Failed to open file " << filename << std::endl;
+#endif
+    return false;
+  }
+
+  // State machine
+  // func -> at -> block -> size -> from -> to
+  //   |             `----------------------|
+  //   `------------------------------------'
+
+  std::string line;
+  enum STATE {
+    IDLE,        // -> FUNC/terminate
+    FUNC,        // -> FUNC_AT
+    FUNC_AT,     // -> BLOCK
+    BLOCK,       // -> BLOCK_SIZE
+    BLOCK_SIZE,  // -> BLOCK_FROM
+    BLOCK_FROM,  // -> BLOCK_TO
+    BLOCK_TO,    // -> IDLE/FUNC_AT
+  } state = IDLE;
+
+  auto parseFile = [](std::string &line, std::string &file,
+                      size_t from) -> uint32_t {
+    auto idx = line.find_last_of(':');
+
+    if (idx == std::string::npos) {
+      return 0;
+    }
+    else {
+      file = std::move(line.substr(from, idx));
+
+      return strtoul(line.substr(idx + 1).c_str(), nullptr, 10);
+    }
+  };
+
+  while (!file.eof()) {
+    Function *current = nullptr;
+    BasicBlock *bb = nullptr;
+
+    // Read one line
+    std::getline(file, line);
+
+    if (state == BLOCK_TO) {
+      if (line.compare(0, 6, "func: ") == 0) {
+        state = IDLE;
+      }
+      else if (line.compare(0, 8, "  block: ") == 0) {
+        state = FUNC_AT;
+      }
+      else {
+        return false;
+      }
+    }
+
+    switch (state) {
+      case IDLE:
+        // Expect 'func: <Function name>'
+        if (line.compare(0, 6, "func: ") != 0) {
+          return false;
+        }
+
+        // Create function entry
+        list.emplace_back(Function());
+        current = &list.back();
+
+        // Store function name
+        current->name = std::move(line.substr(6));
+
+        state = FUNC;
+
+        break;
+      case FUNC:
+        // Expect ' at: [filename:line]'
+        if (line.compare(0, 5, " at: ") != 0) {
+          return false;
+        }
+
+        // Store filename
+        current->at = parseFile(line, current->file, 5);
+
+        state = FUNC_AT;
+
+        break;
+      case FUNC_AT:
+        // Expect ' block: <basic block name>'
+        if (line.compare(0, 8, "  block: ") != 0) {
+          return false;
+        }
+
+        // Create basicblock entry
+        current->blocks.emplace_back(BasicBlock());
+        bb = &current->blocks.back();
+
+        // Store basicblock name
+        bb->name = std::move(line.substr(8));
+
+        state = BLOCK;
+
+        break;
+      case BLOCK:
+        // Expect '  size: <IR count>'
+        if (line.compare(0, 8, "  size: ") != 0) {
+          return false;
+        }
+
+        state = BLOCK_SIZE;
+
+        break;
+      case BLOCK_SIZE:
+        // Expect '  from: [filename:line]'
+        if (line.compare(0, 8, "  from: ") != 0) {
+          return false;
+        }
+
+        // Store source info
+        bb->begin = parseFile(line, bb->from, 8);
+
+        state = BLOCK_FROM;
+
+        break;
+      case BLOCK_FROM:
+        // Expect '  to: [filename:line]'
+        if (line.compare(0, 6, "  to: ") != 0) {
+          return false;
+        }
+
+        // Store source info
+        bb->end = parseFile(line, bb->to, 6);
+
+        state = BLOCK_TO;
+
+        break;
+      default:
+        return false;
+    }
+  }
+
   return true;
 }
 
